a8d3f4f259cb4358d8047c91e5573121d34f6556
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-03 16:33:41 -0500

    :recycle: Update

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 bool isOdd(int n) { return n % 2 == 1; }
 
 int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("Expected more than one argument.\n");
+    return 1;
+  }
+
   for (int i = 1; i < argc; i++) {
     printf("%s\n", isOdd(atoi(argv[i])) ? "true" : "false");
   };
